06960092c8b01dbe399dc9577d165574e8942b22
Vitor Py <vitor@vitorpy.com>
2025-10-28 16:19:17 +0100

    Make WalletRead::utxo_query_height required (remove unimplemented default)
    This fixes the GetTreeState bug where the unimplemented default would cause
    UTXO refresh to fail with 'must specify a block height or ID (hash)' error.
    
    The implementation already exists in zcash_client_sqlite 0.18.9, this just
    makes the trait method properly required.
    
    Backport from commit b0cb1a343 to v0.20.0

--- zcash_client_backend/src/data_api.rs
+++ zcash_client_backend/src/data_api.rs
@@ -1763,9 +1763,7 @@ pub trait WalletRead {
     /// received an output in a mined transaction; therefore, we take that height to be where we
     /// should start searching for UTXOs.
     #[cfg(feature = "transparent-inputs")]
-    fn utxo_query_height(&self, _account: Self::AccountId) -> Result<BlockHeight, Self::Error> {
-        unimplemented!("WalletRead::utxo_query_height must be overridden for wallets to use the `transparent-inputs` feature")
-    }
+    fn utxo_query_height(&self, account: Self::AccountId) -> Result<BlockHeight, Self::Error>;
 
     /// Returns a vector of ephemeral transparent addresses associated with the given
     /// account controlled by this wallet, along with their metadata. The result includes
